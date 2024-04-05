39fddcaac7eea3b6284dc48e07e607aa900a07b3
karthick <mailkarthi90@gmail.com>
2024-04-05 12:37:55 +0530

    correct

--- newgit.tf
+++ newgit.tf
@@ -1,28 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.43.0"
-      
+      source  = "hashicorp/aws"
+      version = "3.44.0"  # Ensure the version matches the one you want to use
     }
   }
 }
 
 provider "aws" {
-    region = "ap-south-1"
-
-  # Configuration options
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "web" {
-
-    ami = "ami-007020fd9c84e18c7"
-    
-    instance_type = "t2.micro"
-    count = 1
-    tags = {
-        key_Name = "ec2final"
-        value = "ec2final1"
-        
-    }
+  ami           = "ami-007020fd9c84e18c7"
+  instance_type = "t2.micro"
+  count         = 1
+  
+  tags = {
+    key_Name = "ec2final"
+    value    = "ec2final1"
+  }
 }
+
